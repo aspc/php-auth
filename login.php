95fafcaa15db7d78b28fea892ddeaa4e45dcf0b9
Matt Dahl <matt.dahl.2013@gmail.com>
2015-05-27 22:31:45 -0400

    Emails error messages.

--- login.php
+++ login.php
@@ -14,7 +14,6 @@
 session_name("ASPC_PHP_SESSION");
 session_set_cookie_params(0, "/", ".pomona.edu");
 session_start();
-error_log("Started session.");
 
 $DJANGO_SESSION_COOKIE_NAME = "sessionid";
 
@@ -22,7 +21,7 @@ $DJANGO_SESSION_COOKIE_NAME = "sessionid";
 # This should never happen in the normal login flow; this will only be triggered if the user
 # manually navigates to https://aspc.pomona.edu/php-auth/login.php himself after already logging in
 if (isset($_SESSION["username"])) {
-	header("refresh:5;url=https://aspc.pomona.edu/" );
+	header("refresh:5;url=https://aspc.pomona.edu/");
 	echo "Whoops, " . $_SESSION["username"] . "! You are already logged in.";
 	echo "Redirecting to <a href=\"https://aspc.pomona.edu/\">https://aspc.pomona.edu/</a>...";
 	die();
@@ -30,7 +29,6 @@ if (isset($_SESSION["username"])) {
 
 # If there is a Django session cookie present, create the session using the Django session data
 elseif (isset($_COOKIE[$DJANGO_SESSION_COOKIE_NAME])) {
-	error_log("Django cookie set.");
 	$django_session_id = $_COOKIE[$DJANGO_SESSION_COOKIE_NAME];
 
 	# Checks if a Django session already exists, and returns the associated user data for the session if it does
@@ -38,32 +36,38 @@ elseif (isset($_COOKIE[$DJANGO_SESSION_COOKIE_NAME])) {
 	try {
 		$command = "python " . getcwd() . "/django_session.py " . escapeshellarg($django_session_id);
 		$result = json_decode(shell_exec($command));
-		error_log("Result decoded.");
 	}
 	catch (Exception $e) {
-		error_log($e->getMessage());
+		mail("digitalmedia@aspc.pomona.edu", "[ASPC] PHP ERROR: login.php:41", (string)$e->getMessage());
 		die();
 	}
 
 	# Evaluates result
 	if (is_object($result) && property_exists($result, "error")) {
 		# Log the error and try logging in again through Django (might cause a redirect loop... lol)
-		error_log($result->error);
+		mail("digitalmedia@aspc.pomona.edu", "[ASPC] PHP ERROR: login.php:48", (string)$result->error);
 		header("Location: https://aspc.pomona.edu/accounts/login/");
 		die();
 	}
+
+	# Performs the PHP $_SESSION login sequence (see the LDAP version in the old login.php)
 	elseif (is_array($result)) {
-		# Performs the PHP $_SESSION login sequence (see the LDAP version in the old jslogin.php)
-		error_log($result["username"] . " logged in from Django.");
 		$username_parts = explode("@", $result[0]); # Array of form [username, suffix]
 
 		# Only log in Pomona students on the PHP side
 		# (5C students shouldn't have access to voting or course reviews, at least yet)
 		if (strpos($username_parts[1], "pomona.edu") !== FALSE) {
 			# The username for users as PHP knows them is still just the Pomona username without the email suffix
-			$_SESSION["username"] = $username_parts[0];
-			$_SESSION["first"] = $result[1];
-			$_SESSION["last"] = $result[2];
+			try {
+				$_SESSION["username"] = $username_parts[0];
+				$_SESSION["first"] = $result[1];
+				$_SESSION["last"] = $result[2];
+			}
+			catch (Exception $e) {
+				mail("digitalmedia@aspc.pomona.edu", "[ASPC] PHP ERROR: login.php:67", (string)$e->getMessage());
+				header("Location: https://aspc.pomona.edu/accounts/login/");
+				die();
+			}
 		}
 
 		# Redirect to desired location, or the mainsite homepage
@@ -77,9 +81,10 @@ elseif (isset($_COOKIE[$DJANGO_SESSION_COOKIE_NAME])) {
 			die();
 		}
 	}
+
+	# Something unexpected happened... Don't know how to handle, so just try logging in again
 	else {
-		# Something unexpected happened... Don't know how to handle, so just try logging in again
-		error_log(var_dump($result));
+		mail("digitalmedia@aspc.pomona.edu", "[ASPC] PHP ERROR: login.php:87", (string)var_dump($result));
 		header("Location: https://aspc.pomona.edu/accounts/login/");
 		die();
 	}
